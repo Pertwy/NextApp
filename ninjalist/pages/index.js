9f4a77e9f66abfc625f5f5ffbe271ec20bd46b73
Pertwy <johnpatrickperkins@gmail.com>
2021-04-17 16:48:16 +0100

    styles and 404 added

--- ninjalist/pages/index.js
+++ ninjalist/pages/index.js
@@ -1,16 +1,16 @@
 import Head from 'next/head'
 import Link from 'next/link'
-import Footer from '../comps/Footer'
-import Navbar from '../comps/Navbar'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div>
+    <div >
 
-        <h1>Home</h1>
-        <p> Why do we use it? It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
-        <Link href="/ninjas" ><a>See Ninja Listing</a></Link>
+        <h1 className={styles.title}>Home</h1>
+        <p className={styles.text}> Why do we use it? It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+        <Link href="/ninjas" >
+          <a className={styles.btn}>See Ninja Listing</a>
+        </Link>
 
     </div>
   )
